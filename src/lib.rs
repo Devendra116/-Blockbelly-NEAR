c82ff89a6199f24875a37ecf6f16f6a5f6cd5020
Devendra116 <chauhandevendra116@gmail.com>
2022-07-13 20:40:17 +0530

    updated token metadata

--- src/lib.rs
+++ src/lib.rs
@@ -34,6 +34,7 @@ pub struct Contract {
     token: FungibleToken,
     metadata: LazyOption<FungibleTokenMetadata>,
     token_allocation:HashMap<String,String>,
+    input_token:String
 }
 
 const DATA_IMAGE_SVG_NEAR_ICON: &str = "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 288 288'%3E%3Cg id='l' data-name='l'%3E%3Cpath d='M187.58,79.81l-30.1,44.69a3.2,3.2,0,0,0,4.75,4.2L191.86,103a1.2,1.2,0,0,1,2,.91v80.46a1.2,1.2,0,0,1-2.12.77L102.18,77.93A15.35,15.35,0,0,0,90.47,72.5H87.34A15.34,15.34,0,0,0,72,87.84V201.16A15.34,15.34,0,0,0,87.34,216.5h0a15.35,15.35,0,0,0,13.08-7.31l30.1-44.69a3.2,3.2,0,0,0-4.75-4.2L96.14,186a1.2,1.2,0,0,1-2-.91V104.61a1.2,1.2,0,0,1,2.12-.77l89.55,107.23a15.35,15.35,0,0,0,11.71,5.43h3.13A15.34,15.34,0,0,0,216,201.16V87.84A15.34,15.34,0,0,0,200.66,72.5h0A15.35,15.35,0,0,0,187.58,79.81Z'/%3E%3C/g%3E%3C/svg%3E";
@@ -50,14 +51,12 @@ pub fn gethash(l1:Vec<String>,l2:Vec<String>)->HashMap<String,String>{
 }
 
 
-
-
 #[near_bindgen]
 impl Contract {
     /// Initializes the contract with the given total supply owned by the given `owner_id` with
     /// default metadata (for example purposes only).
     #[init]
-    pub fn new_default_meta(owner_id: AccountId, total_supply: U128,token_list:Vec<String>,token_alloc:Vec<String>) -> Self {
+    pub fn new_default_meta(owner_id: AccountId, total_supply: U128,token_list:Vec<String>,token_alloc:Vec<String>,input_token:String) -> Self {
         Self::new(
             owner_id,
             total_supply,
@@ -70,7 +69,9 @@ impl Contract {
                 reference_hash: None,
                 decimals: 24,
             },
-            token_list,token_alloc
+            token_list,
+            token_alloc,
+            input_token,
         )
     }
 
@@ -81,7 +82,9 @@ impl Contract {
         owner_id: AccountId,
         total_supply: U128,
         metadata: FungibleTokenMetadata,
-        token_list:Vec<String>,token_alloc:Vec<String>
+        token_list:Vec<String>,
+        token_alloc:Vec<String>,
+        input_token:String
     ) -> Self {
         assert!(!env::state_exists(), "Already initialized");
         metadata.assert_valid();
@@ -89,6 +92,7 @@ impl Contract {
             token: FungibleToken::new(b"a".to_vec()),
             metadata: LazyOption::new(b"m".to_vec(), Some(&metadata)),
             token_allocation:gethash(token_list,token_alloc),
+            input_token
         };
         this.token.internal_register_account(&owner_id);
         this.token.internal_deposit(&owner_id, total_supply.into());
@@ -224,7 +228,7 @@ mod tests {
         let mut context = get_context(accounts(1));
         testing_env!(context.build());
         // fill empty vec
-        let contract = Contract::new_default_meta(accounts(1).into(), TOTAL_SUPPLY.into(),vec![],vec![]);
+        let contract = Contract::new_default_meta(accounts(1).into(), TOTAL_SUPPLY.into(),vec![],vec![],"NEAR".to_string());
         testing_env!(context.is_view(true).build());
         assert_eq!(contract.ft_total_supply().0, TOTAL_SUPPLY);
         assert_eq!(contract.ft_balance_of(accounts(1)).0, TOTAL_SUPPLY);
@@ -243,7 +247,7 @@ mod tests {
         let mut context = get_context(accounts(2));
         testing_env!(context.build());
         // fill empty vec
-        let mut contract = Contract::new_default_meta(accounts(2).into(), TOTAL_SUPPLY.into(),vec![],vec![]);
+        let mut contract = Contract::new_default_meta(accounts(2).into(), TOTAL_SUPPLY.into(),vec![],vec![],"NEAR".to_string());
         testing_env!(context
             .storage_usage(env::storage_usage())
             .attached_deposit(contract.storage_balance_bounds().min.into())
